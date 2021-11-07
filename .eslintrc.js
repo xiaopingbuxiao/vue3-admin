ff48ed6768a032fb8e5c0cadbcb93006ff2d3854
xiaopingbuxiao <1210774823@qq.com>
2021-11-07 17:34:36 +0800

    🛠build: 增加svg构建以及全局组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,35 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/vue3-essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars':'warn',
-    'vue/no-unused-components':'warn'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn",
+    "vue/no-unused-components": "warn",
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "never",
+        objects: "always",
+        imports: "always",
+        exports: "always",
+        functions: "never"
+      }
+    ]
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         mocha: true
       }
     }
   ]
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,32 @@ const path = require('path')
 const childProcess = require('child_process')
 
 module.exports = {
+  chainWebpack (config) {
+    config.module
+      .rule('svg')
+      .exclude.add(path.resolve('src/icons'))
+      .end()
 
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(path.resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end()
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+  },
   configureWebpack: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
-
-      }
-    }
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   },
 
   devServer: {
@@ -23,6 +41,6 @@ module.exports = {
           console.log(data)
         }
       })
-    }
-  }
+    },
+  },
 }
